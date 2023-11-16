e53f91bdcdb89744bc040edb8516fba552015c57
AndreyYuri <andreycaetano2102@gmail.com>
2023-11-16 10:58:22 -0300

    feat: context product by id user and typing

--- src/@types/products/index.ts
+++ src/@types/products/index.ts
@@ -1,38 +1,51 @@
-interface IDefaultRequisition{
-    loading: boolean,
-    error: string,
+interface IDefaultRequisition {
+  loading: boolean;
+  error: string;
 }
 
-export interface IProducts{
-    id: number,
-    name: string,
-    type: string,
-    duration: number,
-    synopsis: string,
-    image: string
+export interface IProducts {
+  id: number;
+  name: string;
+  type: string;
+  duration: number;
+  synopsis: string;
+  image: string;
 }
 
-export interface IProductReview extends IProducts{
-    reviews: {
-        id: number,
-        movieId: number,
-        userId: number,
-        score: number,
-        description: string
-    }[]
+export interface IProductReview extends IProducts {
+  reviews: {
+    id: number;
+    movieId: number;
+    userId: number;
+    score: number;
+    description: string;
+  }[];
 }
 
-export interface IAllProducts extends IDefaultRequisition{
-    allProductsData: IProducts[]
-    loadAllProducts: () => Promise<void>
+export interface IAllProducts extends IDefaultRequisition {
+  allProductsData: IProducts[];
+  loadAllProducts: () => Promise<void>;
 }
 
-export interface IAllProductsReviews extends IDefaultRequisition{
-    allProductReviewData: IProductReview[],
-    loadAllProductsReviews: () => Promise<void>
+export interface IAllProductsReviews extends IDefaultRequisition {
+  allProductReviewData: IProductReview[];
+  loadAllProductsReviews: () => Promise<void>;
 }
 
-export interface IProductByIdStore extends IDefaultRequisition{
-    productData: object,
-    loadProductById: (id: number) => Promise<void>
+export interface IProductByIdStore extends IDefaultRequisition {
+  productData: object;
+  loadProductById: (id: number) => Promise<void>;
+}
+
+export interface IProductByUserIdStore extends IDefaultRequisition {
+  productByUserIdData: IUserReview[];
+  loadProductByUserId: (userId: number, productId: number) => Promise<void>
+}
+
+export interface IUserReview {
+  id: number;
+  movieId: number;
+  userId: number;
+  score: number;
+  description: string;
 }
