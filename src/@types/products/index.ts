cafaa436bd02c840ebc4da20e88f0c30c3f74aab
AndreyYuri <andreycaetano2102@gmail.com>
2023-11-16 14:36:43 -0300

    format: format documents

--- src/@types/products/index.ts
+++ src/@types/products/index.ts
@@ -39,7 +39,7 @@ export interface IProductByIdStore extends IDefaultRequisition {
 
 export interface IProductByUserIdStore extends IDefaultRequisition {
   productByUserIdData: IUserReview[];
-  loadProductByUserId: (userId: number, productId: number) => Promise<void>
+  loadProductByUserId: (userId: number, productId: number) => Promise<void>;
 }
 
 export interface IUserReview {
