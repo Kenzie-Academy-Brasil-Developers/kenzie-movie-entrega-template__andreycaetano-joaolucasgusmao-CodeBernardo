d2acc3db539f49b5ce0bc1b3f0c5bc62279b0d52
João Lucas <joaolucasgusmao@hotmail.com>
2023-11-22 22:45:26 -0300

    feat: Public Routes created

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,17 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { MoviePage } from "../pages/MoviePage";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { PublicRoutes } from "./PublicRoutes";
 
 export const Router = () => {
   return (
     <Routes>
+      <Route element={<PublicRoutes />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" />
+        <Route path="/register" />
+      </Route>
       <Route element={<PrivateRoutes />}>
         <Route path="movie/:id" element={<MoviePage />} />
       </Route>
-     
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" />
-      <Route path="/register" />
       <Route path="*" />
     </Routes>
   );
