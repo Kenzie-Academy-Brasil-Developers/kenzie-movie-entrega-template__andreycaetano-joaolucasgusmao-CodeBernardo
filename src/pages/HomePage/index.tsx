ddad09850f3b6151f336035986aaab3ae4f19c3e
AndreyYuri <andreycaetano2102@gmail.com>
2023-11-23 14:14:20 -0300

    fix: clear homePage and moviePage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { CurrentMovie } from "../../components/CurrentMovie";
 import { DefaultTemplate } from "../../components/DefaultTemplate";
 import { MoviesList } from "../../components/MoviesList";
@@ -6,22 +5,9 @@ import { MoviesList } from "../../components/MoviesList";
 export const HomePage = () => {
   return (
     <>
-      {/* <DefaultTemplate>
-        <section>
-          <CurrentMovie />
-        </section>
-        <section>
-          <ul>
-            <MoviesList />
-          </ul>
-        </section>
-      </DefaultTemplate> */}
-      <Link to={"/movie/1"}>
-        <button>caminho 1</button>
-      </Link>
-      <Link to={"/movie/2"}>
-        <button>caminho 2</button>
-      </Link>
+      <DefaultTemplate>
+
+      </DefaultTemplate>
     </>
   );
 };
--- src/pages/MoviePage/index.tsx
+++ src/pages/MoviePage/index.tsx
@@ -1,13 +1,10 @@
 import { DefaultTemplate } from "../../components/DefaultTemplate";
-import { MovieViewed } from "../../components/MovieViewed";
 
 export const MoviePage = () => {
   return (
     <>
       <DefaultTemplate>
-        <section>
-          <MovieViewed/>
-        </section>
+        
       </DefaultTemplate>
     </>
   );
